0061ac4cb45109f8ec5844d8dd7b6f6ccf29ef60
samhitaargula <39863220+samhitaargula@users.noreply.github.com>
2024-03-31 18:31:43 -0500

    Updating JS

--- Lab3/index.js
+++ Lab3/index.js
@@ -4,28 +4,32 @@ slider.addEventListener("input", calculateTip);
 const billInput = document.getElementById("bill");
 billInput.addEventListener("change", calculateTip);
 
-const error = document.getElementById('error');
+const error = document.getElementById("error");
 
 function calculateTip(){
+    let tipAmount;
+    let totalBill;
+
+    let tipPercent = document.getElementById("tip").value;
+    document.getElementById("tip_percentage").value = `${tipPercent}%`;
+
     let bill1 = document.querySelector("#bill");
     if(isNaN(bill1.value) || bill1.value=='')
     {
         error.innerText = "Enter valid bill amount!";
+        document.getElementById("tip_amount").value = "";
+        document.getElementById("bill_with_tip").value = "";
         return;
     }
 
     error.innerText = "";
 
     let bill = parseFloat(billInput.value);
+    //billInput.value = bill.toFixed(2);
 
-    billInput.value = bill.toFixed(2);
-
-    let tipPercent = document.getElementById("tip").value;
-
-    let tipAmount = parseFloat((bill * (tipPercent/100)).toFixed(2));
-    let totalBill = parseFloat((bill + tipAmount).toFixed(2));
+    tipAmount = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    totalBill = parseFloat((bill + tipAmount).toFixed(2));
     
-    document.getElementById("tip_percentage").value = `${tipPercent}%`;
     document.getElementById("tip_amount").value = `\$ ${tipAmount}`;
     document.getElementById("bill_with_tip").value = `\$ ${totalBill}`;
 }
